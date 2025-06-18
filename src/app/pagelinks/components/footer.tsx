679268317582f49984201fe40c2e6a4d43ea9b97
victor hugo <victoralmeidadev.io@gmail.com>
2025-06-17 22:20:05 -0300

    fix: Remove background color from footer
    This commit removes the black background color from the footer component. This change improves the footer's appearance by allowing it to inherit the background color from its parent container, creating a cleaner and more consistent look.

--- src/app/pagelinks/components/footer.tsx
+++ src/app/pagelinks/components/footer.tsx
@@ -7,7 +7,7 @@ import { CodeXml } from "lucide-react";
 
 const Footer = () => {
     return (
-        <div className=" bg-black items-center flex flex-col  fixed bottom-0 right-[10%] text-center " >
+        <div className="  items-center flex flex-col  fixed bottom-0 right-[10%] text-center " >
             <a href="https://www.instagram.com/victorhhuggoo?igsh=cnZvbHAzNnN2dnQ1">
                 <p className="text-[12px] py-4 flex items-center gap-4 font-[inter] text-center "  >Desenvolvido por @victorhhuggoo 👽 <CodeXml size={18} /></p>
             </a>
