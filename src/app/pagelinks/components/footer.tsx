0905313193e1ebf74b9fd97a25495f7eda534e63
victor hugo <victoralmeidadev.io@gmail.com>
2025-06-17 22:25:45 -0300

    fix: Remove unnecessary gap in footer text
    This commit removes the `gap-4` class from the footer's paragraph element. This adjustment corrects an unintended spacing issue, ensuring the text and icon within the footer are properly aligned and visually appealing.

--- src/app/pagelinks/components/footer.tsx
+++ src/app/pagelinks/components/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     return (
         <div className="  items-center flex flex-col  fixed bottom-0 right-[22%] text-center " >
             <a href="https://www.instagram.com/victorhhuggoo?igsh=cnZvbHAzNnN2dnQ1">
-                <p className="text-[12px] py-4 flex items-center gap-4 font-[inter] text-center "  >Desenvolvido por @victorhhuggoo 👽 <CodeXml size={18} /></p>
+                <p className="text-[12px] py-4 flex items-center  font-[inter] text-center "  >Desenvolvido por @victorhhuggoo 👽<CodeXml size={18} /></p>
             </a>
         </div>
     );
